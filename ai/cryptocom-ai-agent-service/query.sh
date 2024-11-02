a15d95065ac020c3ccdc3808a7972995093da0d0
jonghwan <jonghwan@crypto.com>
2024-11-02 11:09:13 +0900

    Add .gitignore for developer-platform-client-py and create python chatbot file

--- ai/cryptocom-ai-agent-service/query.sh
+++ ai/cryptocom-ai-agent-service/query.sh
@@ -5,10 +5,6 @@ curl -X POST http://localhost:8000/api/v1/cdc-ai-agent-service/query \
   "options": {
     "openAI": {
       "apiKey": "'"$OPENAI_API_KEY"'"
-    },
-    "chainId": 240,
-    "explorer": {
-      "apiKey": "'"$CRONOS_ZKEVM_TESTNET_API"'"
-    }
+    } 
   }
 }'
